71fd7511d0ab4f66b3fdc0d85a48cfda84349cd3
SarinaHamedani <sarinahamedani3@gmail.com>
2025-07-14 20:51:12 -0400

    Add service layer

---
+++ src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,39 @@
+package com.example.demo.student;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Student {
+    private Long id;
+    private String name;
+    private String email;
+    private LocalDate dob;
+    private Integer age;
+
+    public Student(Long id,
+                   String name,
+                   String email,
+                   LocalDate dob,
+                   Integer age) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+        this.age = age;
+    }
+
+    public Student(String name,
+                   String email,
+                   LocalDate dob,
+                   Integer age) {
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+        this.age = age;
+    }
+}
